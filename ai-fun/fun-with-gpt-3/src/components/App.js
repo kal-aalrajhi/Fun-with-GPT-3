4764fb9f85e11e90ff90cad40e12851e0756c76b
Kal <kal.aalrajhi@gmail.com>
2022-05-10 14:48:07 -0600

    Add submit event so users can add responses and have  them appear

--- ai-fun/fun-with-gpt-3/src/components/App.js
+++ ai-fun/fun-with-gpt-3/src/components/App.js
@@ -7,21 +7,29 @@ export class App extends Component {
     constructor() {
         super();
         this.state = {
-            prompts: [
-                { id: 1, prompt: 'Best bagel toppings', response: 'creamcheese, olive, orange jam' },
-                { id: 2, prompt: 'Worst bagel toppings', response: 'pepper, corn bread, grape' },
-                { id: 3, prompt: 'Best pizza toppings', response: 'onion, olive, pineapple' }
-            ]
+            prompts: []
         }
+    }
+
+    addPrompt = (newPrompt) => {
+        this.setState({ prompts: [...this.state.prompts, newPrompt] });
     }
 
     render() {
         return (
             <main className='App container'>
                 <h1>Fun with GPT-3!</h1>
-                <Form />
+                <Form addPrompt={this.addPrompt} />
+                {!this.state.prompts.length && <h2>No prompts yet -- add some!</h2>}
                 <PromptContainer prompts={this.state.prompts} />
             </main>
         )
     }
 }
+
+// Sample Prompt/response
+// [
+//     { id: 1, prompt: 'Best bagel toppings', response: 'creamcheese, olive, orange jam' },
+//     { id: 2, prompt: 'Worst bagel toppings', response: 'pepper, corn bread, grape' },
+//     { id: 3, prompt: 'Best pizza toppings', response: 'onion, olive, pineapple' }
+// ]
--- ai-fun/fun-with-gpt-3/src/components/Form.js
+++ ai-fun/fun-with-gpt-3/src/components/Form.js
@@ -6,7 +6,28 @@ export class Form extends Component {
         super();
         this.state = {
             prompt: ''
+            // response: ''
         }
+    }
+
+    submitPrompt = (event) => {
+        event.preventDefault();
+        const newPrompt = {
+            id: Date.now(),
+            ...this.state
+            // API response here (taken care of by spread operator)
+        }
+        this.props.addPrompt(newPrompt);
+        this.clearInputs();
+    }
+
+    clearInputs = () => {
+        this.setState({prompt: '' }) // response: '' 
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+        // this.setState({ prompt: event.target.value }); ^same as above, just less dynamic
     }
 
     render() {
@@ -17,8 +38,9 @@ export class Form extends Component {
                     placeholder='Enter a prompt here'
                     name='prompt'
                     value={this.state.prompt}
+                    onChange={event => this.handleChange(event)}
                 />
-                <button>Submit</button>
+                <button onClick={event => this.submitPrompt(event)}>Submit</button>
             </form>
         )
     }

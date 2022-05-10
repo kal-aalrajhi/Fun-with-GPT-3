b3754fe9b5f3cf5f676f3a09b0efaf8e415b00cc
Kal <kal.aalrajhi@gmail.com>
2022-05-10 16:44:55 -0600

    Add API call an update state

--- ai-fun/fun-with-gpt-3/src/components/Card.js
+++ ai-fun/fun-with-gpt-3/src/components/Card.js
@@ -10,3 +10,22 @@ export const Card = ( {prompt, response, id, removePrompt} ) => {
         </div>
     )
 }
+
+
+
+
+// const { Configuration, OpenAIApi } = require("openai");
+
+// const configuration = new Configuration({
+//   apiKey: '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',
+// });
+// const openai = new OpenAIApi(configuration);
+
+// const response = await openai.createCompletion("text-curie-001", {
+//   prompt: "Generate animal description for the following animal: Giraffe",
+//   temperature: 0.8,
+//   max_tokens: 200,
+//   top_p: 1,
+//   frequency_penalty: 0,
+//   presence_penalty: 0,
+// });
--- ai-fun/fun-with-gpt-3/src/components/Form.js
+++ ai-fun/fun-with-gpt-3/src/components/Form.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
 import '../styles/Form.css';
+const { Configuration, OpenAIApi } = require("openai");
 
 export class Form extends Component {
     constructor() {
         super();
         this.state = {
-            prompt: ''
-            // response: ''
+            prompt: '',
+            response: ''
         }
+    }
+
+    getAIResponse = () => {
+        const configuration = new Configuration({
+            apiKey: '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',
+          });
+        
+        const openai = new OpenAIApi(configuration);
+
+        openai.createCompletion("text-curie-001", {
+            prompt: `Generate animal description for the following animal: ${this.state.prompt}`,
+            temperature: 0.8,
+            max_tokens: 200,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        })
+        .then((response) => {
+            this.setState({
+                response: `${response.data.choices[0].text}`
+            });
+
+            const newPrompt = {
+                id: Date.now(),
+                ...this.state
+            }
+            this.props.addPrompt(newPrompt);
+            this.clearInputs();
+        })
+        .catch(err => console.log('Error: ', err));
     }
 
     submitPrompt = (event) => {
         event.preventDefault();
-        const newPrompt = {
-            id: Date.now(),
-            ...this.state
-            // API response here (taken care of by spread operator)
-        }
-        this.props.addPrompt(newPrompt);
-        this.clearInputs();
+        this.getAIResponse();
     }
 
     clearInputs = () => {
@@ -45,3 +70,78 @@ export class Form extends Component {
         )
     }
 }
+
+
+
+
+
+
+
+// export class Form extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             prompt: ''
+//             // response: ''
+//         }
+//     }
+
+//     getAIResponse = () => {
+//         const configuration = new Configuration({
+//             apiKey: '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',
+//           });
+        
+//         const openai = new OpenAIApi(configuration);
+
+//         openai.createCompletion("text-curie-001", {
+//             prompt: `Generate animal description for the following animal: ${this.state.prompt}`,
+//             temperature: 0.8,
+//             max_tokens: 200,
+//             top_p: 1,
+//             frequency_penalty: 0,
+//             presence_penalty: 0,
+//         })
+//         .then((response) => {
+//             console.log(response);
+//             this.setState({
+//                 response: response
+//             });
+//         })
+//     }
+
+//     submitPrompt = (event) => {
+//         event.preventDefault();
+//         this.getAIResponse();
+//         const newPrompt = {
+//             id: Date.now(),
+//             ...this.state
+//             // API response here (taken care of by spread operator)
+//         }
+//         this.props.addPrompt(newPrompt);
+//         this.clearInputs();
+//     }
+
+//     clearInputs = () => {
+//         this.setState({prompt: '' }) // response: '' 
+//     }
+
+//     handleChange = (event) => {
+//         this.setState({ [event.target.name]: event.target.value });
+//         // this.setState({ prompt: event.target.value }); ^same as above, just less dynamic
+//     }
+
+//     render() {
+//         return (
+//             <form>
+//                 <input
+//                     type='text'
+//                     placeholder='Enter a prompt here'
+//                     name='prompt'
+//                     value={this.state.prompt}
+//                     onChange={event => this.handleChange(event)}
+//                 />
+//                 <button onClick={event => this.submitPrompt(event)}>Submit</button>
+//             </form>
+//         )
+//     }
+// }

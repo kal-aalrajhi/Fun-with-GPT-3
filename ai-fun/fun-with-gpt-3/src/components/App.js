07dd2ce68b635155db0e7349e3fde3af7c4bb20d
Kal <kal.aalrajhi@gmail.com>
2022-05-10 10:21:34 -0600

    Add sample data to propts array

--- ai-fun/fun-with-gpt-3/src/components/App.js
+++ ai-fun/fun-with-gpt-3/src/components/App.js
@@ -4,7 +4,11 @@ export class App extends Component {
     constructor() {
         super();
         this.state = {
-            prompt: []
+            prompts: [
+                { id: 1, prompt: 'Best bagel toppings', response: 'creamcheese, olive, orange jam' },
+                { id: 2, prompt: 'Worst bagel toppings', response: 'pepper, corn bread, grape' },
+                { id: 3, prompt: 'Best pizza toppings', response: 'onion, olive, pineapple' }
+            ]
         }
     }
 
